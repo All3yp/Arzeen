07f5d7f4cca2386a304537efbe76473215cc6100
All3yp <noyalle@ualberta.ca>
2020-09-18 18:43:11 -0300

    Create Onboarding with CollectionView and using userDefault, set extension Colors

--- Coffee/SceneDelegate.swift
+++ Coffee/SceneDelegate.swift
@@ -9,9 +9,9 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+    
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
@@ -19,26 +19,27 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(frame: UIScreen.main.bounds)
-        window?.rootViewController = UINavigationController(rootViewController: OnboardingViewController())
+        window?.rootViewController = UINavigationController(rootViewController: TabBarViewController())
         window?.makeKeyAndVisible()
         window?.windowScene = windowScene
         
-        let isFirstLaunch = true //Ainda vou fazer a logica do UserDefaults
         // tenta ler variavel no UserDefaults (firstLaunchFlag)
+        let firstLaunchFlag = UserDefaults.standard.bool(forKey: "firstLaunchFlag")
+        
         // se firstLaunchFlag existir e for true, usuário já entrou pelo menos ma vez no App
             //faz nada
         // se nao,
-            // seta variavel firstLaunchFlag no UserDefults com valor true
+        if !firstLaunchFlag {
             // mostra Onboarding
-    
-        if isFirstLaunch {
-            let onBoarding = OnboardingViewController()
+            let onBoarding = CollectionOnboardViewController()
             onBoarding.modalPresentationStyle = .fullScreen
             self.window?.rootViewController?.present(onBoarding, animated: true, completion: nil)
+            
+            // seta variavel firstLaunchFlag no UserDefults com valor true
+            UserDefaults.standard.set(true, forKey: "firstLaunchFlag")
         }
-        
     }
-
+    
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
@@ -46,22 +47,22 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // The scene may re-connect later, as its session was not neccessarily discarded (see
         // `application:didDiscardSceneSessions` instead).
     }
-
+    
     func sceneDidBecomeActive(_ scene: UIScene) {
         // Called when the scene has moved from an inactive state to an active state.
         // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
     }
-
+    
     func sceneWillResignActive(_ scene: UIScene) {
         // Called when the scene will move from an active state to an inactive state.
         // This may occur due to temporary interruptions (ex. an incoming phone call).
     }
-
+    
     func sceneWillEnterForeground(_ scene: UIScene) {
         // Called as the scene transitions from the background to the foreground.
         // Use this method to undo the changes made on entering the background.
     }
-
+    
     func sceneDidEnterBackground(_ scene: UIScene) {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
