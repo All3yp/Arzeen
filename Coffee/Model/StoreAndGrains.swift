6115c7053c2f311829c99af23ef32f11bbd8b060
Brena Amorim <brena.amorim@live.com>
2020-09-30 10:29:18 -0300

    References refreshed

--- Coffee/Model/StoreAndGrains.swift
+++ Coffee/Model/StoreAndGrains.swift
@@ -8,32 +8,43 @@
 
 import Foundation
 
-struct ListOfStores: Codable {
-    let listOfStores: [DataStore]
+struct ListOfResults: Codable {
+    let listOfStores: [ListOfStore]
 }
 
-struct DataStore: Codable {
-    let name: String
-    let location: String
-    let latitude: Float
-    let longitude: Float
-    let grain: Grain
+struct ListOfStore: Codable {
+    var name: String?
+    var location: String?
+    var latitude: Double?
+    var longitude: Double?
+    var grain: Grain?
 }
 
 struct Grain: Codable {
-    let name: String
-    let species: String
-    let origin: String
-    //Origem mais resumida pra tela de vendas?
-    let cultivation: String
-    let flavor: String
-    let varietyOfGrain: String
-    let details: Details
+    
+    var imageName: String?
+    var name: String?
+    var nameOfCoffee: String?
+    var state: String?
+    var country: String?
+    
+    var finalization: Float
+    var contrast: Float
+    var torra: Float
+    
+    var species: String?
+    var origin: String?
+    var cultivation: String?
+    var flavor: String?
+    var varietyOfGrain: String?
+    var originOfGrain: String?
+    var details: Details?
+    
 }
 
 struct Details: Codable {
-    let sensoryProfile: String
-    let altitude: String
-    let body: Int
-    let acidity: Int
+    var sensoryProfile: String?
+    var altitude: String?
+    var body: Double?
+    var acidity: Double?
 }
